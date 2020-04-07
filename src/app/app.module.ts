6400f3dac718f926fc030c3d9c53dc37fb42d88c
SaldanRoman <saldanroman@gmail.com>
2020-04-07 19:25:50 +0300

    todos added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TableComponent } from './components/table/table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { HttpClientModule } from '@angular/common/http';
-import { TodosComponent } from './components/todos/todos.component';
-import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AddTodoFormComponent } from './components/todos/add-todo-form/add-todo-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ModalformComponent } from './components/todos/modalform/modalform.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TableComponent,
-    TodosComponent,
-    LoginComponent,
-    AddTodoFormComponent,
-    ModalformComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -29,9 +17,9 @@ import { ModalformComponent } from './components/todos/modalform/modalform.compo
     MaterialModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
