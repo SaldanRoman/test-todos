43628c5125697a875f0a96d670a170f9ac47a2ca
SaldanRoman <saldanroman@gmail.com>
2020-04-07 19:28:28 +0300

    added modal component

---
+++ src/app/components/todos/modalform/modalform.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Todo, DataService } from 'src/app/services/data.service.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-modalform',
+  templateUrl: './modalform.component.html',
+  styleUrls: ['./modalform.component.scss']
+})
+export class ModalformComponent implements OnInit {
+  // isEditMode = false;
+  @Input() todo: Todo;
+  @Input() isEditMode = false;
+
+  @Output() editedTodo: EventEmitter<Todo> = new EventEmitter();
+  @Output() close: EventEmitter<void> = new EventEmitter();
+
+  form: FormGroup;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(this.todo.title, Validators.required),
+      description: new FormControl(this.todo.description, Validators.required)
+    });
+  }
+
+  closeModal() {
+    this.close.emit();
+  }
+
+  submit() {
+    const year = new Date().getFullYear();
+    const date = new Date().getDate();
+    const month = new Date().getMonth();
+    const hours = new Date().getHours();
+    const min = new Date().getMinutes();
+    const curentDate = `${year}-${month + 1}-${date} | ${hours} : ${min}`;
+
+    this.todo.title = this.form.value.title;
+    this.todo.description = this.form.value.description;
+    this.todo.editDate = curentDate;
+
+    this.dataService.editData(this.todo).subscribe((res: any) => {
+      console.log(res);
+    });
+
+    setTimeout(() => this.closeModal(), 1000);
+  }
+}
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,107 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService, Todo } from 'src/app/services/data.service.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/custom.validators';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[] = [];
+  isModalOn = false;
+  editMode = false;
+  confirmModal = false;
+  addConfirm = false;
+  deleteConfirm = false;
+  idToDeleteFromConfirm: string;
+  modalData: Todo;
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.dataService.getData().subscribe((res) => {
+      this.todos = res;
+    });
+
+    this.form = new FormGroup({
+      title: new FormControl('', CustomValidators.required),
+      description: new FormControl('', CustomValidators.required),
+    });
   }
 
+  newTodoConfirm() {
+    this.confirmModal = true;
+    this.addConfirm = true;
+  }
+
+  removeTodoConfirm(event: any, id: string) {
+    event.stopPropagation();
+    this.idToDeleteFromConfirm = id;
+    this.confirmModal = true;
+    this.deleteConfirm = true;
+  }
+
+  closeConfirm() {
+    this.confirmModal = false;
+    this.addConfirm = false;
+    this.deleteConfirm = false;
+  }
+
+  closeModal() {
+    this.isModalOn = false;
+    this.editMode = false;
+  }
+
+  showModal(data: Todo) {
+    console.log('todos', data);
+    this.modalData = data;
+    this.isModalOn = true;
+  }
+
+  submit() {
+    const year = new Date().getFullYear();
+    const date = new Date().getDate();
+    const month = new Date().getMonth();
+    const hours = new Date().getHours();
+    const min = new Date().getMinutes();
+    const curentDate = `${year}-${month + 1}-${date} | ${hours} : ${min}`;
+
+    const todo = {
+      id: '',
+      title: this.form.value.title,
+      description: this.form.value.description,
+      creationDate: curentDate,
+      editDate: curentDate,
+    };
+    this.dataService.createData(todo).subscribe((res: any) => {
+      todo.id = res.name;
+      this.todos.push(todo);
+    });
+
+    this.form.reset();
+    this.closeConfirm();
+  }
+
+  editTodo(event: any, data: Todo) {
+    event.stopPropagation();
+    this.editMode = true;
+    this.showModal(data);
+  }
+
+  removeTodo() {
+    this.dataService.remove(this.idToDeleteFromConfirm).subscribe(
+      () => {
+        this.todos = this.todos.filter(
+          (todo) => todo.id !== this.idToDeleteFromConfirm
+        );
+        this.idToDeleteFromConfirm = '';
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+    this.closeConfirm();
+  }
 }
