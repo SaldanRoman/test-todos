97f89b4d3bdf76f19c175c44e313f90f7e3758d2
SaldanRoman <saldanroman@gmail.com>
2020-04-07 19:31:55 +0300

    fixed madules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
 import { HttpClientModule } from '@angular/common/http';
+import { TodosComponent } from './components/todos/todos.component';
+import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalformComponent } from './components/todos/modalform/modalform.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    LoginComponent,
+    ModalformComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
